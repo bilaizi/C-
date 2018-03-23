f1deb172bb03c240f8d3be7c97d986a70bbd953b
bilaizi <2512075115@qq.com>
2018-03-24 01:51:02 +0800

    Update util.c

--- util.c
+++ util.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv){
 }
 */
 
-/*
+
 struct person {
     int age;
     char *name;
@@ -55,12 +55,21 @@ static inline void INIT_LIST_HEAD(struct list_head *list){
 // Insert a new entry between two known consecutive entries.
 // This is only for internal list manipulation where we know the prev/next entries already!
 static inline void __list_add(struct list_head *new, struct list_head *prev, struct list_head *next){
-    if (!__list_add_valid(new, prev, next))
-	return;
-    next->prev = new;
-    new->next = next;
-    new->prev = prev;
-    WRITE_ONCE(prev->next, new);
+	if (!__list_add_valid(new, prev, next))
+		return;
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	WRITE_ONCE(prev->next, new);
 }
 
-*/
+/**
+ * list_add - add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it after
+ * Insert a new entry after the specified head. This is good for implementing stacks.
+ */
+static inline void list_add(struct list_head *new, struct list_head *head) {
+	__list_add(new, head, head->next);
+}
+
