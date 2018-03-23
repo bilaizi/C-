1733cd9ba470fcb601a78a276e5f199aa93ffaa7
bilaizi <2512075115@qq.com>
2018-03-23 22:00:07 +0800

    Update helloworld-params.c

--- helloworld-params.c
+++ helloworld-params.c
@@ -34,5 +34,6 @@ MODULE_AUTHOR("Bi Laizi <2512075115@qq.com>");
 MODULE_LICENSE("GPL");
 
 /*
+//To load the hellomodule-params.ko module and feed our parameter, we do the following:
 insmod hellomodule-params.ko a=1 b=0,1,2 c="hello world!"
 */
