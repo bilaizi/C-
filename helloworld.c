6809343d7d8ed156af58d97972835ab7b4a63470
bilaizi <2512075115@qq.com>
2018-03-23 20:41:39 +0800

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,7 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
-static int __init helloworld(void) {
+static int __init helloworld_init(void) {
     pr_info("Start of hello world!\n");
     return 0;
 }
@@ -11,7 +11,7 @@ static void __exit helloworld_exit(void) {
     pr_info("End of the hello world\n");
 }
 
-module_init(hellowolrd_init);
-module_exit(hellowolrd_exit);
+module_init(helloworld_init);
+module_exit(helloworld_exit);
 MODULE_AUTHOR("Bi Laizi <2512075115@qq.com>");
 MODULE_LICENSE("GPL");
