0ec21de1a2bd64c742e6609429f932c8fe2a54a3
bilaizi <2512075115@qq.com>
2018-03-24 02:01:43 +0800

    Update util.c

--- util.c
+++ util.c
@@ -73,3 +73,38 @@ static inline void list_add(struct list_head *new, struct list_head *head) {
 	__list_add(new, head, head->next);
 }
 
+/**
+ * list_add_tail - add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it before
+ * Insert a new entry before the specified head. This is useful for implementing queues.
+ */
+static inline void list_add_tail(struct list_head *new, struct list_head *head) {
+	__list_add(new, head->prev, head);
+}
+
+/*
+ * Delete a list entry by making the prev/next entries point to each other.
+ * This is only for internal list manipulation where we know the prev/next entries already!
+ */
+static inline void __list_del(struct list_head * prev, struct list_head * next) {
+	next->prev = prev;
+	WRITE_ONCE(prev->next, next);
+}
+
+/**
+ * list_del - deletes entry from list.
+ * @entry: the element to delete from the list.
+ * Note: list_empty() on entry does not return true after this, the entry is in an undefined state.
+ */
+static inline void __list_del_entry(struct list_head *entry) {
+	if (!__list_del_entry_valid(entry))
+		return;
+	__list_del(entry->prev, entry->next);
+}
+
+static inline void list_del(struct list_head *entry) {
+	__list_del_entry(entry);
+	entry->next = LIST_POISON1;
+	entry->prev = LIST_POISON2;
+}
