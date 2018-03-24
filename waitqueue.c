c9bca5592cc9d9063fe606a44b2ca6832811f3af
bilaizi <2512075115@qq.com>
2018-03-24 11:16:15 +0800

    Update waitqueue.c

--- waitqueue.c
+++ waitqueue.c
@@ -6,41 +6,36 @@
 #include <linux/delay.h>
 #include<linux/workqueue.h>
 
-static DECLARE_WAIT_QUEUE_HEAD(my_wq);
+static DECLARE_WAIT_QUEUE_HEAD(waitqueue);
 static int condition = 0;
-
 /* declare a work queue*/
-static struct work_struct wrk;
+static struct work_struct workqueue;
 
-static void work_handler(struct work_struct *work)
-{ 
+static void work_handler(struct work_struct *workqueue) { 
     pr_info("Waitqueue module handler %s\n", __FUNCTION__);
     msleep(3000);
     pr_info("Wake up the sleeping module\n");
     condition = 1;
-    wake_up_interruptible(&my_wq);
+    wake_up_interruptible(&waitqueue);
 }
 
-static int __init my_init(void)
-{
-    pr_info("Wait queue example\n");
-
-    INIT_WORK(&wrk, work_handler);
-    schedule_work(&wrk);
-
+static int __init waitqueue_init(void) {
+    pr_info("Wait queue init\n");
+    INIT_WORK(&workqueue, work_handler);
+    schedule_work(&workqueue);
     pr_info("Going to sleep %s\n", __FUNCTION__);
-    wait_event_interruptible(my_wq, condition != 0);
-
+    wait_event_interruptible(waitqueue, condition != 0);
     pr_info("woken up by the work job\n");
     return 0;
 }
 
-void my_exit(void)
-{
-    pr_info("waitqueue example cleanup\n");
+void waitqueue_exit(void) {
+        pr_info("waitqueue cleanup\n");
 }
 
-module_init(my_init);
-module_exit(my_exit);
-MODULE_AUTHOR("John Madieu <john.madieu@gmail.com>");
+module_init(waitqueue_init);
+module_exit(waitqueue_exit);
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Bi Laizi <bilaizi1030@126.com>");
+MODULE_AUTHOR("Bi Peng <2512075115@qq.com>");
+MODULE_DESCRIPTION("wait queue Module");
