27e7fab9e571ff70ce5cfd5626a1260a7ad2af09
bilaizi <2512075115@qq.com>
2018-03-25 18:40:42 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
+// C std headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+// C linux system headers
+#include <fcntl.h>
+#include <sys/select.h>
+#include <unistd.h>
 int main(){
     return 0;
 }
