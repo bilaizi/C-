6d023c188b2ffdc56b7eb0ca5b80f3c1bbc5c817
bilaizi <2512075115@qq.com>
2018-03-25 11:47:54 +0800

    Update irq.c

--- irq.c
+++ irq.c
@@ -1,3 +1,7 @@
 typedef irqreturn_t (*irq_handler_t)(int, void*);
 int request_irq(unsigned int irq, irq_handler_t handler, unsigned long flags, const char* name, void* dev)
 int request_threaded_irq(unsigned int irq, irq_handler_t handler, irq_handler_t thread_fn, unsigned long flags, const char *name, void *dev);
+
+int request_irq(unsigned int irq, irq_handler_t handler, unsigned long flags, const char* name, void* dev){
+    return request_threaded_irq(irq, handler, NULL, flags, name, dev);
+}
