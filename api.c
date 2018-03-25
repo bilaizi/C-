33eaf8c1e646ee80af79ff16b23f66f00e31355d
bilaizi <2512075115@qq.com>
2018-03-25 17:41:30 +0800

    Update api.c

--- api.c
+++ api.c
@@ -2,3 +2,4 @@ unsigned long copy_from_user(void* to, const void __user* from, unsigned long n)
 unsigned long copy_to_user(void __user* to, const void* from, unsigned long n)
 ssize_t (*read)(struct file* filp, char __user* buf, size_t count, loff_t* pos);
 ssize_t (*write)(struct file* filp, const char __user* buf, size_t count, loff_t* pos)
+loff_t  (*llseek)(struct file* filp, loff_t offset, int whence)
