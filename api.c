70bb078b68631c754a6740b1777bb90a4cb7547f
bilaizi <2512075115@qq.com>
2018-03-25 17:34:58 +0800

    Update api.c

--- api.c
+++ api.c
@@ -1,5 +1,5 @@
 
 unsigned long copy_from_user(void* to, const void __user* from, unsigned long n)
 unsigned long copy_to_user(void __user* to, const void* from, unsigned long n)
-ssize_t (*read)(struct file *filp, char __user *buf, size_t count, loff_t *pos);
+ssize_t (*read)(struct file* filp, char __user* buf, size_t count, loff_t* pos);
 ssize_t (*write)(struct file* filp, const char __user* buf, size_t count, loff_t* pos)
