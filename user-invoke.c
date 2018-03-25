643f0117917776f28a338f013ffb4db7f5b5ef5a
bilaizi <2512075115@qq.com>
2018-03-25 12:31:50 +0800

    Update user-invoke.c

--- user-invoke.c
+++ user-invoke.c
@@ -3,9 +3,9 @@
 #include <linux/workqueue.h> /* for work queue */
 #include <linux/kmod.h>
 
-static struct delayed_work initiate_shutdown_work;
+static struct delayed_work delayed_shutdown_work;
 
-static void delayed_shutdown(struct work_struct *work)
+static void delayed_shutdown(struct work_struct* work)
 {
     char *cmd = "/sbin/shutdown";
     char *argv[] = {
@@ -19,23 +19,24 @@ static void delayed_shutdown(struct work_struct *work)
         "PATH=/sbin:/bin:/usr/sbin:/usr/bin",
         NULL,
     };
-
     call_usermodehelper(cmd, argv, envp, 0);
 }
 
-static int __init my_shutdown_init( void )
+static int __init delayed_shutdown_init( void )
 {
-    INIT_DELAYED_WORK(&initiate_shutdown_work, delayed_shutdown);
-    schedule_delayed_work(&initiate_shutdown_work, msecs_to_jiffies(200));
+    INIT_DELAYED_WORK(&delayed_shutdown_work, delayed_shutdown);
+    schedule_delayed_work(&delayed_shutdown_work, msecs_to_jiffies(200));
     return 0;
 }
 
-static void __exit my_shutdown_exit( void )
+static void __exit delayed_shutdown_exit( void )
 {
     return;
 }
 
-module_init( my_shutdown_init );
-module_exit( my_shutdown_exit );
+module_init(delayed_shutdown_init);
+module_exit(delayed_shutdown_exit);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("John Madieu <john.madieu@gmail.com>");
+MODULE_AUTHOR("Bi Laizi <bilaizi1030@126.com>");
+MODULE_AUTHOR("Bi Peng <2512075115@qq.com>");
+MODULE_DESCRIPTION("Trigger a delayed shut down! Module");
