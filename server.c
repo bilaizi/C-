073baafe24113a6f0cceaf1386636c23d64901ec
bilaizi <2512075115@qq.com>
2018-03-26 14:21:51 +0800

    Update server.c

--- server.c
+++ server.c
@@ -34,9 +34,9 @@ void addfd(int epollfd,int fd,bool enable_et){
     setnonblocking(fd);
 }
 
-void lt(epoll_event* events,int number,int epollfd,int listenfd){
+void lt(epoll_event *events, int nfds, int epollfd, int listenfd){
     char buf[BUFFER_SIZE];
-    for(int i = 0;i < number;i++){
+    for(int i = 0; i < nfds; i++){
         int sockfd = events[i].data.fd;
         if(sockfd == listenfd){
             struct sockaddr_in client_address;
@@ -59,9 +59,9 @@ void lt(epoll_event* events,int number,int epollfd,int listenfd){
     }
 }
 
-void et(epoll_event* events,int number,int epollfd,int listenfd){
+void et(epoll_event *events, int nfds, int epollfd, int listenfd){
     char buf[BUFFER_SIZE];
-    for(int i = 0;i < number;i++){
+    for(int i = 0; i < nfds; i++){
         int sockfd = events[i].data.fd;
         if(sockfd == listenfd){
             struct sockaddr_in client_address;
@@ -72,8 +72,8 @@ void et(epoll_event* events,int number,int epollfd,int listenfd){
         else if (events[i].events & EPOLLIN){
             printf("event trigger once\n");
             while(true){
-                memset(buf,'\0',BUFFER_SIZE);
-                int ret = recv(sockfd,buf,BUFFER_SIZE-1,0);
+                memset(buf, '\0', BUFFER_SIZE);
+                int ret = recv(sockfd, buf, BUFFER_SIZE - 1, 0);
                 if(ret < 0){
                     if((errno == EAGAIN )||(errno == EWOULDBLOCK)){
                         printf( "read later\n" );
@@ -100,28 +100,22 @@ int main(int argc,char* argv[]){
     }
     const char* ip = argv[1];
     int port = atoi(argv[2]);
-
     int ret = 0;
     struct sockaddr_in server_address;
     bzero(&server_address,sizeof(server_address));
     server_address.sin_family = AF_INET;
     inet_pton(AF_INET,ip,&server_address.sin_addr);
     server_address.sin_port = htons(port);
-
     int listenfd = socket(PF_INET,SOCK_STREAM,0);
     assert(listenfd >= 0);
-
     ret = bind(listenfd,(struct sockaddr*)&server_address,sizeof(server_address));
     assert(ret != -1);
-
     ret = listen(listenfd, 5);
     assert(ret != -1);
-
     epoll_event events[MAX_EVENT_NUMBER];
     int epollfd = epoll_create(5);
     assert(epollfd != -1);
     addfd(epollfd,listenfd,true);
-
     while(true){
         int ret = epoll_wait(epollfd,events,MAX_EVENT_NUMBER,-1);
         if(ret < 0){
@@ -132,4 +126,5 @@ int main(int argc,char* argv[]){
         //et(events,ret,epollfd,listenfd);
     }
     close(listenfd);
-   
+    return 0;
+}
