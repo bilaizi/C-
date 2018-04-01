b1565755d2e72cf276f60fe1045d34fe21183059
bilaizi <2512075115@qq.com>
2018-04-01 11:31:26 +0800

    Update irq.h

--- irq.h
+++ irq.h
@@ -78,3 +78,27 @@ struct irq_chip {
 	void		(*ipi_send_mask)(struct irq_data *data, const struct cpumask *dest);
 	unsigned long	flags;
 };
+
+/**
+ * struct irq_data - per irq chip data passed down to chip functions
+ * @mask:		precomputed bitmask for accessing the chip registers
+ * @irq:		interrupt number
+ * @hwirq:		hardware interrupt number, local to the interrupt domain
+ * @common:		point to data shared by all irqchips
+ * @chip:		low level interrupt hardware access
+ * @domain:		Interrupt translation domain; responsible for mapping between hwirq number and linux irq number.
+ * @parent_data:	pointer to parent struct irq_data to support hierarchy irq_domain
+ * @chip_data:		platform-specific per-chip private data for the chip methods, to allow shared chip implementations
+ */
+struct irq_data {
+	u32			mask;
+	unsigned int		irq;
+	unsigned long		hwirq;
+	struct irq_common_data	*common;
+	struct irq_chip		*chip;
+	struct irq_domain	*domain;
+#ifdef	CONFIG_IRQ_DOMAIN_HIERARCHY
+	struct irq_data		*parent_data;
+#endif
+	void			*chip_data;
+};
