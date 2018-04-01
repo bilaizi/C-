96f23547ac5cd331247dfe2514f902902491ca18
bilaizi <2512075115@qq.com>
2018-04-01 20:55:43 +0800

    Update input-polldev.h

--- input-polldev.h
+++ input-polldev.h
@@ -1,19 +1,19 @@
 #ifndef _INPUT_POLLDEV_H
 #define _INPUT_POLLDEV_H
-
 #include <linux/input.h>
 #include <linux/workqueue.h>
-
 /**
  * struct input_polled_dev - simple polled input device
  * @private: private driver data.
  * @open: driver-supplied method that prepares device for polling(enabled the device and maybe flushes device state).
  * @close: driver-supplied method that is called when device is no longer being polled. Used to put device into low power mode.
  * @poll: driver-supplied method that polls the device and posts input events (mandatory).
- * @poll_interval: specifies how often the poll() method should be called. Defaults to 500 msec unless overridden when registering the device.
+ * @poll_interval: specifies how often the poll() method should be called.
+ 	Defaults to 500 msec unless overridden when registering the device.
  * @poll_interval_max: specifies upper bound for the poll interval. Defaults to the initial value of @poll_interval.
  * @poll_interval_min: specifies lower bound for the poll interval. Defaults to 0.
- * @input: input device structure associated with the polled device. Must be properly initialized by the driver (id, name, phys, bits).
+ * @input: input device structure associated with the polled device. 
+ 	Must be properly initialized by the driver (id, name, phys, bits).
  *
  * Polled input device provides a skeleton for supporting simple input
  * devices that do not raise interrupts but have to be periodically
