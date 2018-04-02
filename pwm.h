c35d6cacf6eb68edb776d6f6359d4bce3d890363
bilaizi <2512075115@qq.com>
2018-04-02 15:29:58 +0800

    Update pwm.h

--- pwm.h
+++ pwm.h
@@ -50,3 +50,25 @@ struct pwm_ops {
 #endif
 	struct module *owner;
 };
+
+/**
+ * struct pwm_chip - abstract a PWM controller
+ * @dev: device providing the PWMs
+ * @list: list node for internal use
+ * @ops: callbacks for this PWM controller
+ * @base: number of first PWM controlled by this chip
+ * @npwm: number of PWMs controlled by this chip
+ * @pwms: array of PWM devices allocated by the framework
+ * @of_xlate: request a PWM device given a device tree PWM specifier
+ * @of_pwm_n_cells: number of cells expected in the device tree PWM specifier
+ */
+struct pwm_chip {
+	struct device *dev;
+	struct list_head list;
+	const struct pwm_ops *ops;
+	int base;
+	unsigned int npwm;
+	struct pwm_device *pwms;
+	struct pwm_device * (*of_xlate)(struct pwm_chip *pc, onst struct of_phandle_args *args);
+	unsigned int of_pwm_n_cells;
+};
